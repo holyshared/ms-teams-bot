a87f954583739a94440537d9618f802cc7f7e377
holyshared <holy.shared.design@gmail.com>
2020-07-15 14:53:38 +0900

    add sources

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+
+app.get('/callback', (req, res) => {
+  const code = req.query.code;
+  console.log('code------------------');
+  console.log(code);
+  res.status(200).end('Ok');
+});
+
+app.get('/token', (req, res, next) => {
+  console.log('GET /token');
+  console.log(req.body);
+  res.status(200).end('Ok');
+});
+
+app.listen(process.env.PORT || 3000);
